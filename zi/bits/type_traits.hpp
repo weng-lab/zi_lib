674d1ce3afcc0ce934ecea022e87e73a66483824
Arjan van der Velde <vandervelde.ag@gmail.com>
2014-11-29 14:21:58 -0500

    ok, keep it enabled for glibc

--- zi/bits/type_traits.hpp
+++ zi/bits/type_traits.hpp
@@ -85,8 +85,7 @@ using ZI_TYPE_TRAITS_NAMESPACE::aligned_storage;
 
 // disabled as per http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2010/n3142.html
 // => clang 3.4 sets __GXX_EXPERIMENTAL_CXX0X__ but does not have std::has_trivial_.*
-#ifdef DISABLED__GXX_EXPERIMENTAL_CXX0X__
-
+#if defined(__GXX_EXPERIMENTAL_CXX0X__) && defined(__GLIBCXX__)
 template< typename T >
 struct has_trivial_constructor: std::has_trivial_default_constructor< T > {};
 
